21d5ed7dd4c1f91f29482ed0315ca28233beb6a9
fargelus <ddraudred@gmail.com>
2025-06-19 20:42:41 +0300

    Method extracted

--- main.go
+++ main.go
@@ -13,6 +13,18 @@ type Task struct {
 	name string
 }
 
+func checkCommandWithArg(cmd string, arg string) {
+	if !isValidCommand(cmd) {
+		fmt.Fprintf(os.Stderr, "Error: Unknown command\n")
+		os.Exit(1)
+	}
+
+	if !isCommandArgExpected(cmd, arg) {
+		fmt.Fprintf(os.Stderr, "Error: Unknown arg for command <%s>\n", cmd)
+		os.Exit(1)
+	}
+}
+
 func isValidCommand(cmd string) bool {
 	valid := false
 	for _, item := range commands {
@@ -47,19 +59,11 @@ func isCommandArgExpected(cmd string, arg string) bool {
 
 func main() {
 	cmd := os.Args[1]
-	if !isValidCommand(cmd) {
-		fmt.Fprintf(os.Stderr, "Error: Unknown command\n")
-		os.Exit(1)
-	}
-
 	arg := ""
 	if len(os.Args) > 2 {
 		arg = os.Args[2]
 	}
-	if !isCommandArgExpected(cmd, arg) {
-		fmt.Fprintf(os.Stderr, "Error: Unknown arg for command <%s>\n", cmd)
-		os.Exit(1)
-	}
+	checkCommandWithArg(cmd, arg)
 
 	if cmd == "add" {
 		task := Task{id: 1, name: arg}
